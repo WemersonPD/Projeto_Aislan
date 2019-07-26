0a51cc331e2a92d9e50b876dbcaa100727f10df6
WemersonPD <wemersonpatrick733@gmail.com>
2019-07-25 19:51:08 -0500

    menu completo

--- menu.py
+++ menu.py
@@ -1,2 +1,19 @@
-print('Ola mundo!')
-print('testando')
+
+from cores import vermelho, amarelo, verde
+import os
+from jogando import jogar
+
+os.system("cls" if os.name == "nt" else "clear")
+print(amarelo("MENU"))
+print(amarelo("1 - JOGAR"))
+print(amarelo("2 - SAIR"))
+request = input(verde("ESCOLHA: "))
+while not( request == "1" or request == "2"):
+    os.system("cls" if os.name == "nt" else "clear")
+    print(vermelho("OPÇÃO INVÁLIDA"))
+    print(amarelo("1 - JOGAR"))
+    print(amarelo("2 - SAIR"))
+    request = input(verde("ESCOLHA: "))
+
+if request == "1":
+    jogar()
